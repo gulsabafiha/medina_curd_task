1ff7373815d9d76381f3fc2798150b53a7985569
Gulsaba Fiha <sabafiha111@gmail.com>
2022-09-27 00:47:11 +0600

    weather type api validation added

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from apps.weather.models import WeatherType
 from apps.weather.serializers import WeatherTypeSerializers
+from rest_framework.response import Response
+from django.http import Http404
 
 
 # Create your views here.
@@ -10,5 +12,10 @@ class WeatherTypeViewSet(viewsets.ModelViewSet):
     serializer_class = WeatherTypeSerializers
 
     def get_queryset(self):
+        print(self.request.user.is_staff)
         if self.request.user.is_staff:
             return super().get_queryset()
+        else:
+            raise Http404
+            # queryset=WeatherType.objects.none()
+            # return Response({'detail':'You do not have the permissions to get the data'})
